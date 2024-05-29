27491f88849c2d576334da71ce899dbb4afa877e
ThirafiQaedi <Qaedi68@gmail.com>
2024-05-29 16:14:54 +0700

    new update

--- Capstone/chart.js
+++ Capstone/chart.js
@@ -1,19 +1,44 @@
-function loadJson(){
-    fetch("data_france.json")
-                .then((res) => {
-                   return res.json();
-                })
-                .then((data) => 
-                      console.log(data))
-                .catch((error) => 
-                       console.error("Unable to fetch data:", error));
+
+async function loadFranceJSON() {
+  const response = await fetch('data_france.json');
+  const data = await response.json();
+
+  return data;
 }
-loadJson()
+
+async function loadAllJSON() {
+  const response = await fetch('data_blmcleaning.json');
+  const data = await response.json();
+
+  return data;
+}
+
+async function loadData(){
+  const franceD = await loadFranceJSON() 
+  
+  let filtered = franceD.filter(filtering)
+
+  let groupedBy = Object.groupBy(filtered, d => d.Customer_Gender)
+  let sum =0;
+  groupedBy.F.forEach(femaledata => {
+    sum += femaledata.Profit
+  });
+  debugger
+
+}
+function filtering(countrybikesales) {
+  return countrybikesales.Month == "January"
+}
+
+
+loadData()
+
 
 function handleOnChangeMonth(element){
     let month = element.value
 
 }
+
 
 const ages = [32, 33, 16, 40];
 const result = ages.filter(checkAdult);
@@ -22,51 +47,36 @@ function checkAdult(age) {
   return age >= 18;
 }
 
+document
+  .getElementById("toggleDropdownbulan")
+  .addEventListener("click", function () {
+    var dropdownContent = document.getElementById("dropdownbulan");
+    if (dropdownContent.style.display === "none") {
+      dropdownContent.style.display = "block";
+    } else {
+      dropdownContent.style.display = "none";
+    }
+  });
 
-const ttpch = document.getElementById('TotalProfitChart');
-const tpsC = document.getElementById('TotalProfitAgeChart');
+// dropdown tahun
+document
+  .getElementById("toggleDropdowntahun")
+  .addEventListener("click", function () {
+    var dropdownContent = document.getElementById("dropdowntahun");
+    if (dropdownContent.style.display === "none") {
+      dropdownContent.style.display = "block";
+    } else {
+      dropdownContent.style.display = "none";
+    }
+  });
+
+// chart js <!-- section profit in eropa -->
+
+
 const tppcc = document.getElementById('TotalProfitPCategoryChart');
 const tpgc = document.getElementById('TotalProfitGenderChart');
 
       
-new Chart(ttpch, {
-  type: 'bar',
-  data: {
-    labels: ['United States', 'Australia', 'United Kingdom', 'Canada', 'germany', 'France'],
-    datasets: [{
-      label: '# Profit',
-      data: [6187169, 3991425, 2402989, 2167470, 2058238, 2045254],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(255, 205, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(201, 203, 207, 0.2)'
-      ],
-      borderColor: [
-        'rgb(255, 99, 132)',
-        'rgb(255, 159, 64)',
-        'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-        'rgb(54, 162, 235)',
-        'rgb(153, 102, 255)',
-        'rgb(201, 203, 207)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    indexAxis: 'y',
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
-
 
 new Chart(tpgc, {
   type: 'doughnut',
@@ -79,3 +89,360 @@ new Chart(tpgc, {
   }
 });
 
+new Chart(tppcc, {
+  type: 'bar',
+  data: {
+    labels: ['United States', 'Australia', 'United Kingdom'],
+    datasets: [{
+      label: 'Profit',
+      data: [6187169, 3991425, 2402989],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+
+
+const ctx = document.getElementById("myChart");
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ['United States', 'Australia', 'United Kingdom', 'Canada', 'germany', 'France'],
+    datasets: [
+      {
+        axis: "y",
+        label: "Profit",
+        data: [6187169, 3991425, 2402989, 2167470, 2058238, 2045254],
+        fill: false,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(201, 203, 207, 0.2)",
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    indexAxis: "y",
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+// total profit in eropa grop by age
+const age = document.getElementById("age-Chart-eropa").getContext("2d");
+const umur = ["0-10", "11-20", "21-30", "31-40", "41-50"];
+const data = {
+  labels: umur,
+  datasets: [
+    {
+      label: "Age Distribution",
+      data: [15, 25, 30, 20, 10], // Jumlah individu dalam setiap kelompok umur
+      backgroundColor: "rgba(75, 192, 192, 0.2)", // Warna area
+      borderColor: "rgba(75, 192, 192, 1)", // Warna garis tepi
+      borderWidth: 1,
+    },
+  ],
+};
+// Konfigurasi grafik area
+const config = {
+  type: "line",
+  data: data,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+};
+// Membuat grafik menggunakan konfigurasi yang telah ditentukan
+const ageChart = new Chart(age, config);
+
+//
+
+// total profit in eropa grop by categry
+const ctxcategory = document
+  .getElementById("category-chart-eropa")
+  .getContext("2d");
+const productData = {
+  // Data penjualan produk berdasarkan kategori
+  labels: ["Accessories", "Bikes", "Clothing"],
+  datasets: [
+    {
+      label: "Sales by Category",
+      data: [10091084, 5898052, 2863409], // Jumlah penjualan dalam setiap kategori
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)", // Warna untuk Accessories
+        "rgba(54, 162, 235, 0.2)", // Warna untuk Bikes
+        "rgba(255, 205, 86, 0.2)", // Warna untuk Clothing
+  
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)", // Warna border untuk Accessories
+        "rgba(54, 162, 235, 1)", // Warna border untuk Bikes
+        "rgba(255, 205, 86, 1)", // Warna border untuk Clothing
+       
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+// Konfigurasi grafik bar
+const productConfig = {
+  type: "bar",
+  data: productData,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+};
+// Membuat grafik menggunakan konfigurasi yang telah ditentukan
+const productChart = new Chart(ctxcategory, productConfig);
+
+// total profit data q3 q4 france
+const ctxfrance = document.getElementById("salesChart").getContext("2d");
+// Data penjualan bulanan
+const salesData = {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [
+    {
+      label: "Sales",
+      data: [1000, 1500, 1200, 1800, 2000, 1700, 1900], // Data penjualan per bulan
+      fill: false,
+      borderColor: "rgba(75, 192, 192, 1)", // Warna garis
+      borderWidth: 1,
+    },
+  ],
+};
+// Konfigurasi grafik garis
+const salesConfig = {
+  type: "line",
+  data: salesData,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+};
+// Membuat grafik menggunakan konfigurasi yang telah ditentukan
+const salesChart = new Chart(ctxfrance, salesConfig);
+
+// Total Profit by Sub Category Q3 & Q4 In France
+const ctxPSC = document.getElementById("productSalesChart").getContext("2d");
+const sDfrance = {
+  // Data penjualan produk
+  labels: [
+    "Tires and Tubes",
+    "Road Bikes",
+    "Touring Bikes",
+    "Jerseys",
+    "Bottles and Cages",
+    "Hydration Packs",
+    "Shorts",
+    "Gloves",
+    "Mountain Bikes",
+    "Fenders",
+    "Vests",
+    "Bike Stands",
+    "Bike Racks",
+    "Caps",
+    "Cleaners",
+    "Socks",
+  ],
+  datasets: [
+    {
+      label: "Product Sales",
+      data: [
+        1500, 1200, 800, 1000, 600, 400, 1100, 900, 700, 500, 300, 200, 400,
+        300, 200, 100,
+      ], // Data penjualan untuk setiap produk
+      backgroundColor: "rgba(75, 192, 192, 0.2)", // Warna area di dalam batang
+      borderColor: "rgba(75, 192, 192, 1)", // Warna border batang
+      borderWidth: 1,
+    },
+  ],
+};
+
+// Konfigurasi grafik batang
+const sFfrance = {
+  type: "bar",
+  data: sDfrance,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+};
+
+// Membuat grafik menggunakan konfigurasi yang telah ditentukan
+const salesCt = new Chart(ctxPSC, sFfrance);
+
+// Profit age group by product category Q3 & Q4 in France
+const ctxq3q4france = document
+  .getElementById("salesChartq3q4")
+  .getContext("2d");
+// Data penjualan berdasarkan kategori dan kelompok umur
+const salesDataq3q4 = {
+  labels: ["Accessories", "Bikes", "Clothing"],
+  datasets: [
+    {
+      label: "Adults (35-64)",
+      data: [500, 800, 600], // Data penjualan untuk kelompok umur Adults (35-64)
+      backgroundColor: "rgba(255, 99, 132, 0.2)", // Warna area di dalam batang
+      borderColor: "rgba(255, 99, 132, 1)", // Warna border batang
+      borderWidth: 1,
+    },
+    {
+      label: "Young Adults (25-34)",
+      data: [300, 700, 400], // Data penjualan untuk kelompok umur Young Adults (25-34)
+      backgroundColor: "rgba(54, 162, 235, 0.2)", // Warna area di dalam batang
+      borderColor: "rgba(54, 162, 235, 1)", // Warna border batang
+      borderWidth: 1,
+    },
+    {
+      label: "Youth (<25)",
+      data: [200, 500, 300], // Data penjualan untuk kelompok umur Youth (<25)
+      backgroundColor: "rgba(255, 205, 86, 0.2)", // Warna area di dalam batang
+      borderColor: "rgba(255, 205, 86, 1)", // Warna border batang
+      borderWidth: 1,
+    },
+    {
+      label: "Seniors (64+)",
+      data: [100, 300, 200], // Data penjualan untuk kelompok umur Seniors (64+)
+      backgroundColor: "rgba(75, 192, 192, 0.2)", // Warna area di dalam batang
+      borderColor: "rgba(75, 192, 192, 1)", // Warna border batang
+      borderWidth: 1,
+    },
+  ],
+};
+
+// Konfigurasi grafik batang
+const salesConfigq3q4 = {
+  type: "bar",
+  data: salesDataq3q4,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+};
+// Membuat grafik menggunakan konfigurasi yang telah ditentukan
+const salesChartq3q4 = new Chart(ctxq3q4france, salesConfigq3q4);
+
+// Total Profit By product category Q3 & Q4 in France
+const ctxProductSalesChart = document
+  .getElementById("psfranceq3q4")
+  .getContext("2d");
+// Data penjualan untuk kategori Accessories, Bikes, dan Clothing
+const productSalesData = {
+  labels: ["Accessories", "Bikes", "Clothing"],
+  datasets: [
+    {
+      label: "Product Sales",
+      data: [1500, 1200, 800], // Data penjualan untuk setiap kategori
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)", // Warna area di dalam batang untuk Accessories
+        "rgba(54, 162, 235, 0.2)", // Warna area di dalam batang untuk Bikes
+        "rgba(255, 205, 86, 0.2)", // Warna area di dalam batang untuk Clothing
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)", // Warna border batang untuk Accessories
+        "rgba(54, 162, 235, 1)", // Warna border batang untuk Bikes
+        "rgba(255, 205, 86, 1)", // Warna border batang untuk Clothing
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+// Konfigurasi grafik batang
+const productSalesConfig = {
+  type: "bar",
+  data: productSalesData,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true, // Mulai sumbu y dari 0
+      },
+    },
+  },
+};
+// Membuat grafik menggunakan konfigurasi yang telah ditentukan
+const productSalesChart = new Chart(ctxProductSalesChart, productSalesConfig);
+
+// Total Profit customer gender By product category Q3 & Q4 in France
+// Konteks dari elemen canvas
+const ctxGenderSalesChart = document
+  .getElementById("genderSCq3q4")
+  .getContext("2d");
+
+// Data penjualan untuk kategori Accessories, Bikes, dan Clothing berdasarkan jenis kelamin
+const genderSalesData = {
+  labels: ["Accessories", "Bikes", "Clothing"],
+  datasets: [
+    {
+      label: "Male",
+      data: [500, 800, 600], // Data penjualan untuk laki-laki
+      backgroundColor: "rgba(54, 162, 235, 0.2)", // Warna area di dalam batang
+      borderColor: "rgba(54, 162, 235, 1)", // Warna border batang
+      borderWidth: 1,
+    },
+    {
+      label: "Female",
+      data: [300, 700, 400], // Data penjualan untuk perempuan
+      backgroundColor: "rgba(255, 99, 132, 0.2)", // Warna area di dalam batang
+      borderColor: "rgba(255, 99, 132, 1)", // Warna border batang
+      borderWidth: 1,
+    },
+  ],
+};
+
+// Konfigurasi grafik batang
+const genderSalesConfig = {
+  type: "bar",
+  data: genderSalesData,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true, // Mulai sumbu y dari 0
+      },
+    },
+  },
+};
+
+// Membuat grafik menggunakan konfigurasi yang telah ditentukan
+const genderSalesChart = new Chart(ctxGenderSalesChart, genderSalesConfig);
